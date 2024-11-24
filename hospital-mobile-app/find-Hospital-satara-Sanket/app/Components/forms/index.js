6b37aa5c733566e84d6d49dcaf9c512241dfc881
Sarthak <saarsaach30@gmail.com>
2024-11-24 21:06:14 +0530

    Update logo pic asset

--- hospital-mobile-app/find-Hospital-satara-Sanket/app/Components/forms/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Form} from './Form';
-export { default as FormField} from './FormField';
-export { default as ErrorMessage} from './ErrorMessage';
-export { default as SubmitButton} from './SubmitButton';
-export { default as FormPicker} from './FormPicker';
-export {default as FormImagePicker} from './FormImagePicker';
